5239f9a5fc8a2f1ee27c4f8af64e32a55b804918
Amandhi <amandhi.kahawita@efrei.net>
2020-11-14 13:53:30 +0100

    Second commit

---
+++ app/src/main/java/fr/efrei/tp1/AddCityActivity.java
@@ -0,0 +1,106 @@
+package fr.efrei.tp1;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.efrei.tp1.bo.City;
+import fr.efrei.tp1.preferences.AppPreferences;
+import fr.efrei.tp1.repository.CityRepository;
+
+final public class AddCityActivity
+    extends AppCompatActivity
+    implements OnClickListener
+{
+
+  //The tag used into this screen
+  public static final String TAG = AddCityActivity.class.getSimpleName();
+
+  //private EditText
+  private EditText CityName;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_city);
+
+    //We retrieve the EditText References
+    CityName = findViewById(R.id.editTextCityName);
+
+
+
+    //We add the listener on the "save" button
+    findViewById(R.id.save).setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v)
+  {
+
+
+
+    //we first retrieve city name
+    final String cityName = CityName.getEditableText().toString();
+
+
+    //We check if all entries are valid (not null and not empty)
+    final boolean canAddCity = checkFormEntries(cityName);
+
+    if (canAddCity == true)
+    {
+      //We add the city to the list and we reset the form
+      saveCity(cityName);
+      saveLogin();
+      resetForm();
+    }
+    else
+    {
+      //we display a log error and a Toast
+      Log.w(AddCityActivity.TAG, "Cannot add the city");
+      Toast.makeText(this, R.string.cannot_add_city, Toast.LENGTH_SHORT).show();
+    }
+  }
+
+
+
+  private void saveLogin()
+  {
+    //We save only if there is something to save
+    if (TextUtils.isEmpty(CityName.getText()) == false)
+    {
+      AppPreferences.saveUserLogin(this, CityName.getText().toString());
+    }
+
+    //we return to the previous screen
+    onBackPressed();
+  }
+
+  private void resetForm()
+  {
+    CityName.setText(null);
+
+  }
+
+  private void saveCity(String cityName)
+  {
+    CityRepository.getInstance(this).addCity(new City(cityName));
+  }
+
+  private boolean checkFormEntries(String cityName)
+  {
+    return TextUtils.isEmpty(cityName) == false;
+  }
+
+
+
+
+
+}
